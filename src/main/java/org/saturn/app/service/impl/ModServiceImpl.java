d3a74ba0adb9d4dfe6930f356eb33f5a8edc7e68
ab <arteom.balanuta@gmail.com>
2025-05-13 03:17:51 +0300

    typo fix

--- src/main/java/org/saturn/app/service/impl/ModServiceImpl.java
+++ src/main/java/org/saturn/app/service/impl/ModServiceImpl.java
@@ -135,7 +135,7 @@ public class ModServiceImpl extends OutService implements ModService {
 
   @Override
   public void forceColor(String target, String hexcolor) {
-    enqueueRawMessageForSending(String.format("{ \"cmd\": \"forcecolor\", \"nick\": \"%s\", color: \"%s\" }", target, hexcolor));
+    enqueueRawMessageForSending(String.format("{ \"cmd\": \"forcecolor\", \"nick\": \"%s\", \"color\": \"%s\" }", target, hexcolor));
   }
 
   @Override
